4f7f60773a92f0b7878e53a0176f03e0c5112bc2
Ankur Oberoi <aoberoi@gmail.com>
2019-12-14 08:56:05 -0800

    add listener to hungry messages which offers user a way to start pie orders

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+app.event('message', ({ event, say }) => {
+  if (event.text.includes('hungry')) {
+    say({
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `<@${event.user}>, we've got fresh homemade pies.`,
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Start an order',
+            },
+          },
+        },
+      ],
+    });
+  }
+});
+
 (async () => {
   await app.start(process.env.PORT || 3000);
   console.log('⚡️ Bolt app is running!');
